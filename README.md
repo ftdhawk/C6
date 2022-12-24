f52c25d49bfc251e56e9c4dbe8a77918c8b1cb66
ftdhawk <ninisq@gmail.com>
2022-12-25 00:58:54 +0800

    设置IDE环境

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C6
 《C Primer Plus》第6版学习
+熟悉VS Code书写C语言代码
+熟悉Github
---
+++ chap0/a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;
+    scanf("%d", &a);
+    printf("a = %d\n", a);
+    return 0;
+}
